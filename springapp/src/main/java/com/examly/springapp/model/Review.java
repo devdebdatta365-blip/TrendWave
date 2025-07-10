4a62bf72aa0e71425933575afcd47ad2d3e7bc91
debdatta20 <debdattapal2017@gmail.com>
2025-07-10 17:41:35 +0000

    review class updated

--- springapp/src/main/java/com/examly/springapp/model/Review.java
+++ springapp/src/main/java/com/examly/springapp/model/Review.java
@@ -9,8 +9,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Review {
     @Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -27,56 +35,5 @@ public class Review {
 	private User user;
 	@ManyToOne
 	@JoinColumn(name="productId")
-	private Product product;
-	public Long getReviewId() {
-		return reviewId;
-	}
-	public void setReviewId(Long reviewId) {
-		this.reviewId = reviewId;
-	}
-	public String getReviewText() {
-		return reviewText;
-	}
-	public void setReviewText(String reviewText) {
-		this.reviewText = reviewText;
-	}
-	public Integer getRating() {
-		return rating;
-	}
-	public void setRating(Integer rating) {
-		this.rating = rating;
-	}
-	public LocalDate getDate() {
-		return date;
-	}
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-	public Review() {
-	}
-	public Review(Long reviewId, String reviewText, Integer rating, LocalDate date, User user, Product product) {
-		this.reviewId = reviewId;
-		this.reviewText = reviewText;
-		this.rating = rating;
-		this.date = date;
-		this.user = user;
-		this.product = product;
-	}
-
-	
-
-
-    
+	private Product product;    
 }
