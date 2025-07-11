812a27ceae348fbb31c5e28e7efbc8823627ca99
debdatta20 <debdattapal2017@gmail.com>
2025-07-11 09:19:49 +0000

    SpringBoot done

--- springapp/src/main/java/com/examly/springapp/repository/OrderRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/OrderRepo.java
@@ -10,12 +10,8 @@ import com.examly.springapp.model.Order;
 
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Long>{
-<<<<<<< HEAD
-    @Query("Select p from Order p where p.user.userId=?1")
-=======
 
     @Query("select p from Order p where p.user.userId=?1")
->>>>>>> 515a50307a2868087cc1dc06e5b4945ffd95763c
     List<Order> getOrderList(long userId);
     
 }
