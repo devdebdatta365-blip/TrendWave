1ebf5a8cc125534701db2cd351f91968eebdd041
Saurabhraj2002 <srajs2924@gmail.com>
2025-07-11 19:18:28 +0000

    security done(still testing)

--- springapp/src/main/java/com/examly/springapp/config/UserPrinciple.java
+++ springapp/src/main/java/com/examly/springapp/config/UserPrinciple.java
@@ -1,5 +1,62 @@
 package com.examly.springapp.config;
 
-public class UserPrinciple {
-    
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.examly.springapp.model.User;
+
+import io.jsonwebtoken.lang.Collections;
+
+public class UserPrinciple implements UserDetails {
+    // private final User user;
+
+    private String email;
+    private String password;
+    private List<GrantedAuthority> authorities = new ArrayList<>();
+
+    public UserPrinciple(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getUserRole()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
