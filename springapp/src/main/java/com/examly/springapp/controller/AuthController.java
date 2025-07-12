b630e32c6f0278d4c3990e0576cfe927e8207709
Saurabhraj2002 <srajs2924@gmail.com>
2025-07-12 10:04:29 +0000

    validation in spring

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -1,29 +1,23 @@
 package com.examly.springapp.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.examly.springapp.config.JwtUtils;
 import com.examly.springapp.model.LoginDTO;
 import com.examly.springapp.model.User;
 import com.examly.springapp.repository.UserRepo;
 import com.examly.springapp.service.UserService;
 
-import jakarta.jws.soap.SOAPBinding.Use;
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin("*")
@@ -55,7 +49,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity<String> loginUser(@Valid @RequestBody LoginDTO loginDTO){
         String token = userService.loginUser(loginDTO);
         if(token!=null){
             return new ResponseEntity<>(token, HttpStatus.CREATED);
---
+++ springapp/src/main/java/com/examly/springapp/exceptions/ValidationExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.examly.springapp.exceptions;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((org.springframework.validation.FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
