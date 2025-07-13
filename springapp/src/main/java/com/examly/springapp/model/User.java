76147239ab5007a97059628c1222dcc248dd92df
Saurabhraj2002 <srajs2924@gmail.com>
2025-07-13 16:03:34 +0000

    all validations added(server side)

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -8,11 +8,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.*;
+
+// import org.hibernate.engine.jdbc.Size;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -28,15 +34,30 @@ public class User {
 	private Long userId;
 
 	@NotNull(message = "Email is required")
+	@Email(message = "Invalid email format")
+	@NotBlank(message = "Email is required")
 	@Column(name="email")
 	private String email;
+
+	@NotBlank(message = "Password is required")
 	@NotNull(message = "Password is required")
+	@Size(min = 8, message = "Password must be at least 8 characters")
 	@Column(name="password")
 	private String password;
+
+    @NotBlank(message = "Username is required")
+	@NotNull(message = "Username is required")
+	@Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
 	@Column(name="username")
 	private String userName;
+
+    @NotBlank(message = "Mobile number is required")
+	@NotNull(message = "Mobile number is required")
+	@Pattern(regexp = "\\d{10}", message = "Mobile number must be 10 digits")
 	@Column(name="mobile_number")
 	private String mobileNumber;
+
+
 	@Column(name = "user_role")
 	private String userRole;
 	// @OneToMany(mappedBy = "user",cascade=CascadeType.ALL)
