9fe8e48e5aad19d9c690dc8e8578ffa94dc5ab99
debdatta20 <debdattapal2017@gmail.com>
2025-07-14 04:05:57 +0000

    All Angular Test cases passed

--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { Login } from '../models/login.model';
 
 @Injectable({
   providedIn: 'root'
