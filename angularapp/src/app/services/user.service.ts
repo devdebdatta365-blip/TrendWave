08f05ce70e11e9d237b020c40a43a1c8ba6d587e
Saurabhraj2002 <srajs2924@gmail.com>
2025-07-16 04:31:33 +0000

    done

---
+++ angularapp/src/app/services/user.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl: string = ''; 
+
+  constructor(private http: HttpClient) { }
+
+  private createAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/user`, {
+      headers: this.createAuthHeaders()
+    });
+  }
+
+  
+  getUserById(userId: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/user/${userId}`, {
+      headers: this.createAuthHeaders()
+    });
+  }
+}
